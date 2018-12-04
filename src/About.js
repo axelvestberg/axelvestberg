96a2a2c36874f8dd4d8d0424542e57e7c4e3f4ef
Axel Vestberg <axel.vestberg@gmail.com>
2018-12-04 17:41:58 +0100

    added some text and sitemap

--- src/About.js
+++ src/About.js
@@ -5,6 +5,12 @@ class About extends Component {
         return (
             <div>
                 <h1>About</h1>
+                <p id="about-text">I began working on this website in December 2018. I had been trying out a lot of
+                    different techs in web development but never actually had my own domain to play
+                    around with. So here we are. It is still a mystery what will become of this site.
+                    Right now the idea is to be a sort of display of smaller programs I've built. But also a
+                    sort of portfolio that I can refer too in addition to my extremely messy github.
+                </p>
             </div>
         );
     }
